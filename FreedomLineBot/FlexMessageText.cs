d0cb82e8a25b69fbd35c689ca9faafb722402984
rikupin <41769991+rikupin@users.noreply.github.com>
2020-09-21 02:04:37 +0900

    フレックスメッセージをコード化

--- FreedomLineBot/FlexMessageText.cs
+++ FreedomLineBot/FlexMessageText.cs
@@ -1,330 +1,197 @@
-﻿namespace FreedomLineBot
+﻿using Line.Messaging;
+
+namespace FreedomLineBot
 {
     class FlexMessageText
     {
-        public static string FlexJsonCheckContinue =
-@"{
-  ""type"": ""flex"",
-  ""contents"": {
-    ""type"": ""bubble"",
-    ""body"": {
-      ""type"": ""box"",
-      ""layout"": ""vertical"",
-      ""contents"": [
+        public static BubbleContainer Flex_Check_Continue()
         {
-          ""type"": ""text"",
-          ""text"": ""継続希望しますか?"",
-          ""size"": ""xl"",
-          ""weight"": ""bold""
-        },
-        {
-          ""type"": ""box"",
-          ""layout"": ""baseline"",
-          ""contents"": [
+            return new BubbleContainer()
             {
-              ""type"": ""text"",
-              ""text"": ""期限"",
-              ""flex"": 1,
-              ""size"": ""sm"",
-              ""color"": ""#aaaaaa""
-            },
+                Body = new BoxComponent()
+                {
+                    Layout = BoxLayout.Vertical,
+                    Contents = new IFlexComponent[]
+                    {
+                        new TextComponent()
+                        {
+                            Text = "継続希望しますか？",
+                            Weight = Weight.Bold,
+                            Size = ComponentSize.Xl
+                        },
+                        new BoxComponent()
+                        {
+                            Layout = BoxLayout.Baseline,
+                            Spacing = Spacing.Sm,
+                            Contents = new IFlexComponent[]
+                            {
+                                new TextComponent()
+                                {
+                                    Text = "期限",
+                                    Size = ComponentSize.Sm,
+                                    Color = ColorCode.FromRgb(102, 102, 102)
+                                },
+                                new TextComponent()
+                                {
+                                    Text = "日曜日 22:00 まで",
+                                    Size = ComponentSize.Sm,
+                                    Wrap = true,
+                                    Color = ColorCode.FromRgb(102, 102, 102)
+                                }
+                            }
+                        },
+                        new TextComponent()
+                        {
+                            Text = "BOTの反応は数秒から数十秒遅れることがあります。何度も押す必要はありませんが、数分経っても返答がない場合は再度押していただくか、管理メンバーに連絡してください。",
+                            Wrap = true,
+                            Size = ComponentSize.Sm,
+                            Color = ColorCode.FromRgb(102, 102, 102)
+                        },
+                        new TextComponent()
+                        {
+                            Text = "実施期間終了の前日、または前々日に意思表示をされていないメンバーさん宛にメンションをつけて、管理メンバーがGTにてご連絡をします。実施期間終了後、継続を希望しないメンバーさんを、管理メンバーが退会大会に出場させます。ブロック非表示削除等は、各自自由に判断してください。",
+                            Wrap = true,
+                            Size = ComponentSize.Sm,
+                            Color = ColorCode.FromRgb(102, 102, 102)
+                        }
+                    }
+                },
+                Footer = new BoxComponent()
+                {
+                    Layout = BoxLayout.Horizontal,
+                    Contents = new IFlexComponent[]
+                    {
+                        new ButtonComponent()
+                        {
+                            Action = new MessageTemplateAction("継続希望","継続確認"),
+                            Style = ButtonStyle.Primary
+                        },
+                        new ButtonComponent()
+                        {
+                            Action = new UriTemplateAction("ルールを見る","https://rikupin.github.io/FreedomSite/Rules"),
+                            Style = ButtonStyle.Secondary
+                        }
+                    }
+                }
+
+            };
+        }
+        public static BubbleContainer Flex_Greeting()
+        {
+            return new BubbleContainer()
             {
-              ""type"": ""text"",
-              ""text"": ""日曜日 22:00 まで"",
-              ""flex"": 5,
-              ""size"": ""sm"",
-              ""wrap"": true,
-              ""color"": ""#666666""
-            }
-          ],
-          ""spacing"": ""sm""
-        },
-        {
-              ""type"": ""text"",
-              ""text"": ""BOTの反応は数秒から数十秒遅れることがあります。何度も押す必要はありませんが、数分経っても返答がない場合は再度押していただくか、管理メンバーに連絡してください。"",
-              ""flex"": 5,
-              ""wrap"": true,
-              ""size"": ""sm"",
-              ""color"": ""#666666""
-            
-        },
-        {
-              ""type"": ""text"",
-              ""text"": ""実施期間終了の前日、または前々日に意思表示をされていないメンバーさん宛にメンションをつけて、管理メンバーがGTにてご連絡をします。実施期間終了後、継続を希望しないメンバーさんを、管理メンバーが退会大会に出場させます。ブロック非表示削除等は、各自自由に判断してください。"",
-              ""flex"": 5,
-              ""wrap"": true,
-              ""size"": ""sm"",
-              ""color"": ""#666666""
+                Body = new BoxComponent()
+                {
+                    Layout = BoxLayout.Vertical,
+                    Contents = new IFlexComponent[]
+                    {
+                        new TextComponent()
+                        {
+                            Text = "こんにちは",
+                            Weight = Weight.Bold,
+                            Size = ComponentSize.Xl
+                        },
+                        new TextComponent()
+                        {
+                            Text = "ルールを確認、メンバー全員を登録してください。当Botは登録不要です。",
+                            Wrap = true,
+                            Size=ComponentSize.Md,
+                            Color = ColorCode.FromRgb(102,102,102)
+                        }
+                    }
+                },
+                Footer = new BoxComponent()
+                {
+                    Layout = BoxLayout.Horizontal,
+                    Spacing = Spacing.Sm,
+                    Contents = new IFlexComponent[]
+                    {
+                        new ButtonComponent()
+                        {
+                            Action = new UriTemplateAction("ルール","https://rikupin.github.io/FreedomSite/Rules"),
+                            Style = ButtonStyle.Primary
+                        },
+                        new ButtonComponent()
+                        {
+                            Action = new UriTemplateAction("FAQ","https://rikupin.github.io/FreedomSite/FAQs"),
+                            Style = ButtonStyle.Secondary,
+                            Margin = Spacing.Md
+                        }
+                    }
+                }
+
+            };
         }
-      ]
-    },
-    ""footer"": {
-      ""type"": ""box"",
-      ""layout"": ""vertical"",
-      ""contents"": [
+        public static BubbleContainer Flex_Faq()
         {
-          ""type"": ""button"",
-          ""action"": {
-            ""type"": ""message"",
-            ""label"": ""継続希望"",
-            ""text"":""継続希望""
-          },
-          ""height"": ""sm"",
-          ""style"": ""primary""
-        }
-      ],
-      ""flex"": 0,
-      ""spacing"": ""sm""
-    }
-  },
-  ""altText"": ""継続確認""
-}";
-        public static string FlexJsonGreeting =
-@"{
-  ""type"": ""flex"",
-  ""contents"": {
-    ""type"": ""bubble"",
-    ""body"": {
-      ""type"": ""box"",
-      ""layout"": ""vertical"",
-      ""contents"": [
-        {
-          ""type"": ""text"",
-          ""text"": ""こんにちはฅ(｡•ㅅ•｡ฅ"",
-          ""size"": ""xl"",
-          ""weight"": ""bold""
-        },
-        {
-          ""type"": ""box"",
-          ""layout"": ""baseline"",
-          ""contents"": [
+            return new BubbleContainer()
             {
-              ""type"": ""text"",
-              ""text"": ""ルールを確認、メンバー全員を登録してください。当botは登録不要です。"",
-              ""flex"": 5,
-              ""size"": ""md"",
-              ""wrap"": true,
-              ""color"": ""#666666""
-            }
-          ],
-          ""spacing"": ""sm""
+                Body = new BoxComponent()
+                {
+                    Layout = BoxLayout.Vertical,
+                    Contents = new IFlexComponent[]
+                    {
+                        new TextComponent()
+                        {
+                            Text = "いま一度FAQを確認してくださいφ(>ω<*)☆",
+                            Size = ComponentSize.Md,
+                            Wrap = true
+                        },
+                        new ButtonComponent()
+                        {
+                            Action = new UriTemplateAction("FAQを見る","https://rikupin.github.io/FreedomSite/FAQs"),
+                            Style = ButtonStyle.Secondary
+                        }
+                    }
+                }
+            };
         }
-      ]
-    },
-    ""footer"": {
-      ""type"": ""box"",
-      ""layout"": ""vertical"",
-      ""contents"": [
+        public static BubbleContainer Flex_Rule()
         {
-          ""type"": ""button"",
-          ""action"": {
-            ""type"": ""uri"",
-            ""label"": ""ルールを見る"",
-            ""uri"":""https://rikupin.github.io/Freedom_Site/Rules""
-          },
-          ""height"": ""sm"",
-          ""style"": ""secondary""
-        },
-{
-          ""type"": ""button"",
-          ""action"": {
-            ""type"": ""uri"",
-            ""label"": ""FAQを見る"",
-            ""uri"":""https://rikupin.github.io/Freedom_Site/FAQs""
-          },
-          ""height"": ""sm"",
-          ""style"": ""secondary""
-        }
-      ],
-      ""flex"": 0,
-      ""spacing"": ""sm""
-    }
-  },
-  ""altText"": ""こんにちは""
-}";
-        public static string FlexJsonFAQs =
-@"{
-  ""type"": ""flex"",
-  ""contents"": {
-    ""type"": ""bubble"",
-    ""body"": {
-      ""type"": ""box"",
-      ""layout"": ""vertical"",
-      ""contents"": [
-        {
-          ""type"": ""box"",
-          ""layout"": ""baseline"",
-          ""contents"": [
+            return new BubbleContainer()
             {
-              ""type"": ""text"",
-              ""text"": ""いま一度FAQを確認してくださいφ(>ω<*)☆"",
-              ""flex"": 5,
-              ""size"": ""md"",
-              ""wrap"": true
-            }
-          ],
-          ""spacing"": ""sm""
+                Body = new BoxComponent()
+                {
+                    Layout = BoxLayout.Vertical,
+                    Contents = new IFlexComponent[]
+                    {
+                        new TextComponent()
+                        {
+                            Text = "いま一度ルールを確認してくださいφ(>ω<*)☆",
+                            Size = ComponentSize.Md,
+                            Wrap = true
+                        },
+                        new ButtonComponent()
+                        {
+                            Action = new UriTemplateAction("ルールを見る","https://rikupin.github.io/FreedomSite/Rules"),
+                            Style = ButtonStyle.Secondary
+                        }
+                    }
+                }
+            };
         }
-      ]
-    },
-    ""footer"": {
-      ""type"": ""box"",
-      ""layout"": ""vertical"",
-      ""contents"": [
-{
-          ""type"": ""button"",
-          ""action"": {
-            ""type"": ""uri"",
-            ""label"": ""FAQを見る"",
-            ""uri"":""https://rikupin.github.io/Freedom_Site/FAQs""
-          },
-          ""height"": ""sm"",
-          ""style"": ""secondary""
-        }
-      ],
-      ""flex"": 0,
-      ""spacing"": ""sm""
-    }
-  },
-  ""altText"": ""FAQ""
-}";
-        public static string FlexJsonRules =
-@"{
-  ""type"": ""flex"",
-  ""contents"": {
-    ""type"": ""bubble"",
-    ""body"": {
-      ""type"": ""box"",
-      ""layout"": ""vertical"",
-      ""contents"": [
+        public static BubbleContainer Flex_Continue_Checked(string ID)
         {
-          ""type"": ""box"",
-          ""layout"": ""baseline"",
-          ""contents"": [
+            return new BubbleContainer()
             {
-              ""type"": ""text"",
-              ""text"": ""いま一度ルールを確認してくださいφ(> ω < *)☆"",
-              ""flex"": 5,
-              ""size"": ""md"",
-              ""wrap"": true
-            }
-          ],
-          ""spacing"": ""sm""
+                Body = new BoxComponent()
+                {
+                    Layout = BoxLayout.Vertical,
+                    Action = new UriTemplateAction("ルールを見る", "https://rikupin.github.io/FreedomSite/Rules"),
+                    Contents = new IFlexComponent[]
+                    {
+                        new TextComponent()
+                        {
+                            Text = "継続希望を確認しました。"
+                        },
+                        new TextComponent()
+                        {
+                            Text = $"管理用 : {ID}",
+                            Size = ComponentSize.Sm
+                        }
+                    }
+                }
+            };
         }
-      ]
-    },
-    ""footer"": {
-      ""type"": ""box"",
-      ""layout"": ""vertical"",
-      ""contents"": [
-{
-          ""type"": ""button"",
-          ""action"": {
-            ""type"": ""uri"",
-            ""label"": ""ルールを見る"",
-            ""uri"":""https://rikupin.github.io/Freedom_Site/Rules""
-          },
-          ""height"": ""sm"",
-          ""style"": ""secondary""
-        }
-      ],
-      ""flex"": 0,
-      ""spacing"": ""sm""
-    }
-  },
-  ""altText"": ""Rules""
-}";
-        public static string FlexJsonFAQsWithCat =
-@"{
-  ""type"": ""flex"",
-  ""contents"": {
-    ""type"": ""bubble"",
-    ""body"": {
-      ""type"": ""box"",
-      ""layout"": ""vertical"",
-      ""contents"": [
-        {
-          ""type"": ""box"",
-          ""layout"": ""baseline"",
-          ""contents"": [
-            {
-              ""type"": ""text"",
-              ""text"": ""いま一度FAQを確認してくださいにゃ"",
-              ""flex"": 5,
-              ""size"": ""md"",
-              ""wrap"": true
-            }
-          ],
-          ""spacing"": ""sm""
-        }
-      ]
-    },
-    ""footer"": {
-      ""type"": ""box"",
-      ""layout"": ""vertical"",
-      ""contents"": [
-{
-          ""type"": ""button"",
-          ""action"": {
-            ""type"": ""uri"",
-            ""label"": ""FAQを見る"",
-            ""uri"":""https://rikupin.github.io/Freedom_Site/FAQs""
-          },
-          ""height"": ""sm"",
-          ""style"": ""secondary""
-        }
-      ],
-      ""flex"": 0,
-      ""spacing"": ""sm""
-    }
-  },
-  ""altText"": ""FAQ""
-}";
-        public static string FlexJsonRulesWithCat =
-@"{
-  ""type"": ""flex"",
-  ""contents"": {
-    ""type"": ""bubble"",
-    ""body"": {
-      ""type"": ""box"",
-      ""layout"": ""vertical"",
-      ""contents"": [
-        {
-          ""type"": ""box"",
-          ""layout"": ""baseline"",
-          ""contents"": [
-            {
-              ""type"": ""text"",
-              ""text"": ""いま一度ルールを確認してくださいにゃ"",
-              ""flex"": 5,
-              ""size"": ""md"",
-              ""wrap"": true
-            }
-          ],
-          ""spacing"": ""sm""
-        }
-      ]
-    },
-    ""footer"": {
-      ""type"": ""box"",
-      ""layout"": ""vertical"",
-      ""contents"": [
-{
-          ""type"": ""button"",
-          ""action"": {
-            ""type"": ""uri"",
-            ""label"": ""ルールを見る"",
-            ""uri"":""https://rikupin.github.io/Freedom_Site/Rules""
-          },
-          ""height"": ""sm"",
-          ""style"": ""secondary""
-        }
-      ],
-      ""flex"": 0,
-      ""spacing"": ""sm""
-    }
-  },
-  ""altText"": ""Rules""
-}";
     }
 }
