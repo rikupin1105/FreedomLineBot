3269944284acbe8d073f52bd70c9b5300bd4d6f2
rikupin <41769991+rikupin@users.noreply.github.com>
2020-09-30 15:26:37 +0900

    Memberを移動

--- UpdateMemberName/Database.cs
+++ UpdateMemberName/Database.cs
@@ -53,15 +53,5 @@ namespace UpdateMemberName
                 }
             } while (iterator.HasMoreResults);
         }
-        public class Member
-        {
-            public string id { get; set; }
-            public string group = "freedom";
-            public string name { get; set; }
-            public string newername { get; set; }
-            public string joinedDate { get; set; }
-            public string check { get; set; }
-            public string postScript { get; set; }
-        }
     }
 }
