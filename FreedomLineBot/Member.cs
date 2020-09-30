a9ed2eb057e92ea97471bc5d489afd08a105180d
rikupin <41769991+rikupin@users.noreply.github.com>
2020-09-30 15:26:49 +0900

    Create Member.cs

---
+++ FreedomLineBot/Member.cs
@@ -0,0 +1,13 @@
+﻿namespace FreedomLineBot
+{
+    public class Member
+    {
+        public string id { get; set; }
+        public string group = "freedom";
+        public string name { get; set; }
+        public string newername { get; set; }
+        public string joinedDate { get; set; }
+        public string check { get; set; }
+        public string postScript { get; set; }
+    }
+}
