569f2c927e6eb796c3ca53c0c0e2ad01a65b4e03
rikupin <41769991+rikupin@users.noreply.github.com>
2021-01-08 15:04:27 +0900

    Update FlexMessageText.cs

--- FreedomLineBot/FlexMessageText.cs
+++ FreedomLineBot/FlexMessageText.cs
@@ -64,7 +64,7 @@ namespace FreedomLineBot
                         },
                         new ButtonComponent()
                         {
-                            Action = new UriTemplateAction("ルールを見る","https://rikupin1105.github.io/FreedomSite/"),
+                            Action = new UriTemplateAction("ルールを見る","https://rikupin1105.github.io/FreedomSite/Rules"),
                             Style = ButtonStyle.Secondary,
                             Height = ButtonHeight.Sm
                         }
@@ -105,13 +105,13 @@ namespace FreedomLineBot
                     {
                         new ButtonComponent()
                         {
-                            Action = new UriTemplateAction("ルール","https://rikupin1105.github.io/FreedomSite/"),
+                            Action = new UriTemplateAction("ルール","https://rikupin1105.github.io/FreedomSite/Rules"),
                             Style = ButtonStyle.Primary,
                             Height = ButtonHeight.Sm
                         },
                         new ButtonComponent()
                         {
-                            Action = new UriTemplateAction("FAQ","https://rikupin1105.github.io/FreedomSite/faq"),
+                            Action = new UriTemplateAction("FAQ","https://rikupin1105.github.io/FreedomSite/FAQs"),
                             Style = ButtonStyle.Secondary,
                             Height = ButtonHeight.Sm,
                             Margin = Spacing.Md
@@ -133,7 +133,7 @@ namespace FreedomLineBot
                     {
                         new ButtonComponent()
                         {
-                            Action = new UriTemplateAction("FAQを見る", "https://rikupin1105.github.io/FreedomSite/faq"),
+                            Action = new UriTemplateAction("FAQを見る", "https://rikupin1105.github.io/FreedomSite/FAQs"),
                             Style = ButtonStyle.Secondary,
                             Height = ButtonHeight.Sm
                         }
