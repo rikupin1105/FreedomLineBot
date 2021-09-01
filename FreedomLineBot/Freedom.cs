7dde2f120cf61f0d1ddac0543a6aa37a55a7b5c1
kaminaka rikuya <rikupin1105@gmail.com>
2021-09-02 00:26:25 +0900

    udpate

--- FreedomLineBot/Freedom.cs
+++ FreedomLineBot/Freedom.cs
@@ -7,8 +7,8 @@ namespace FreedomLineBot
 {
     public class Freedom
     {
-        public static Sender sender_admin = new Sender("管理メンバー", "https://raw.githubusercontent.com/rikupin1105/FreedomSite/master/IMG/admin.jpg");
-        public static Sender sender_cat = new Sender("ฅ(=✧ω✧=)ฅﾆｬﾆｬｰﾝ✧", "https://raw.githubusercontent.com/rikupin1105/FreedomSite/master/IMG/cat.jpg");
+        public static MessageSender sender_admin = new MessageSender("管理メンバー", "https://raw.githubusercontent.com/rikupin1105/FreedomSite/master/IMG/admin.jpg");
+        public static MessageSender sender_cat = new MessageSender("ฅ(=✧ω✧=)ฅﾆｬﾆｬｰﾝ✧", "https://raw.githubusercontent.com/rikupin1105/FreedomSite/master/IMG/cat.jpg");
         public static Database Database { get; set; }
         public static List<string> Admin_Users = Environment.GetEnvironmentVariable("ADMIN_USER").Split(',').ToList();
         public static string groupId = Environment.GetEnvironmentVariable("GroupId");
